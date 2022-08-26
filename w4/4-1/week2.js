a91653b64e69725e7ad6af6cbdde9b4f360b3e6b
Andrew <97459102+AyyyeJae@users.noreply.github.com>
2022-08-25 20:25:33 -0400

    Regex assignment

--- w4/4-1/week2.js
+++ w4/4-1/week2.js
@@ -8,6 +8,7 @@ const Form = document.getElementById('form');
 // feedback fields
 const warning = document.getElementById('justLettingYouKnow')
 const LogElement = document.getElementById('logs');
+const ValElement = document.getElementById('validate');
 const infoElement = document.getElementById('info');
 const FnError = document.getElementById('fn-error');
 const LnError = document.getElementById('ln-error');
@@ -20,6 +21,14 @@ const LNameLbl = document.getElementById('lNameLbl');
 const EmailLbl = document.getElementById('emailLbl');
 const PnLbl = document.getElementById('pnLbl');
 
+// Fill in default data to the form.
+fName.value='Andrew';
+lName.value='Castro';
+email.value='nonya@business.com';
+phone.value='4012223333';
+
+
+////--------- --------- ALL DAH WORK --------- ---------////
 
 // Listen for events on the submit click
 Form.addEventListener('submit', (e)=>
@@ -29,6 +38,7 @@ Form.addEventListener('submit', (e)=>
 
    //Feedback log to store the messages
    let feedback = []
+   
    // if the input is empty or null (repeat for LName, Email, & Phone)
    if (FName.value ==='' || FName.value == null)
    {
@@ -48,7 +58,7 @@ Form.addEventListener('submit', (e)=>
       FNameLbl.classList.remove('lblError');
 
       // Display the submitted value.
-      infoElement.innerText = FName.value;
+      // infoElement.innerText = FName.value;   /** Removed for the Regex assignment that requires a different output method. */
    }
 
    if (LName.value ==='' || LName.value == null)
@@ -62,7 +72,7 @@ Form.addEventListener('submit', (e)=>
       LnError.innerText = '';
       LNameLbl.classList.remove('lblError');
 
-      infoElement.innerText += ' ' + LName.value;
+      // infoElement.innerText += ' ' + LName.value;
    }
 
    if (Email.value ==='' || Email.value == null)
@@ -76,7 +86,7 @@ Form.addEventListener('submit', (e)=>
       EmError.innerText = '';
       EmailLbl.classList.remove('lblError');
 
-      infoElement.innerText += ' ' + Email.value;
+      // infoElement.innerText += ' ' + Email.value;
    }
 
    if (Phone.value ==='' || Phone.value == null)
@@ -90,7 +100,7 @@ Form.addEventListener('submit', (e)=>
       PnError.innerText = '';
       PnLbl.classList.remove('lblError');
 
-      infoElement.innerText += ' ' + Phone.value;
+      // infoElement.innerText += ' ' + Phone.value;
    }
 
    // If feedback contains ANY error log, prevent the user from submitting.
@@ -99,7 +109,7 @@ Form.addEventListener('submit', (e)=>
       console.log('A field(s) was left empty.')
       e.preventDefault()
       // show user in confirmation div.
-      LogElement.innerText = feedback.join(', ');
+      // LogElement.innerText = feedback.join(', ');
    }
    else
    {
@@ -107,5 +117,105 @@ Form.addEventListener('submit', (e)=>
       Form.classList.add('hide')
       warning.classList.add('hide')
    }
+
+
+   // Call Validation functions
+   validateFName();
+   validateLName();
+   validateEmail();
+   validatePhone();
+
+   // Declare an array to store ALL the form data.
+   var peopleArray = [];
+   // push it in the array.
+   peopleArray.push({ First: fName.value, Last: lName.value, Email: email.value ,Phone: phone.value});
+
+  // -> Now for the loop. Remember that all of this will only act upon being submitted and meeting the criteria above.
+  // Nothing can be sent to the form or array without the submit button working. Submit button won't work without the criteria being met first.
+
+  // declare variable
+	var str = ``
+	for(let i =0; i<peopleArray.length; i++)
+	{
+		// for each VALUE inside the people array -> 
+		for(value in peopleArray[i])
+		{
+
+			str += `<p>`
+			str += `${value}: ${peopleArray[i][value]}`
+			str += `</p>`
+		}
+		//close the div
+		str +=`</div>`
+	}
+   // send it to the log div.
+	logs.innerHTML = str;
 })
+
+
+
+////--------- --------- FUNCTION LIBRARY --------- ---------////
+
+// First name validation
+// declare function
+function validateFName(){
+   // first name regex 
+   var regName = /[-'a-zA-Z]$/;
+   // get fName element 
+   var name = document.getElementById('fName').value;
+   // If the regex finds an error
+   if(!regName.test(name)){
+      // alert them
+       alert('Please fill in or do not enter special characters in the first name, Jay.');
+       document.getElementById('fName').focus();
+       return false;
+       
+   }else{
+       // do nothing if right.
+       return true;
+   }
+}
+
+// Last name validation
+function validateLName(){
+   var regName = /[-'a-zA-Z]$/;
+   var name2 = document.getElementById('lName').value;
+   if(!regName.test(name2)){
+       alert('Please fill in or do not enter special characters in the last name, Jay.');
+       document.getElementById('lName').focus();
+       return false;
+   }else{
+       //
+       return true;
+   }
+}
+
+// Email validation
+function validateEmail(){
+   var regName = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+   var Email = document.getElementById('email').value;
+   if(!regName.test(Email)){
+       alert('Your email was inputted incorrectly...');
+       document.getElementById('email').focus();
+       return false;
+   }else{
+       //
+       return true;
+   }
+}
+
+// Phone validation
+function validatePhone(){
+   var regName = /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/;
+   var Phone = document.getElementById('phone').value;
+   if(!regName.test(Phone)){
+       alert('Your Phone number can only contain NUMBERS!!!! 😡');
+       document.getElementById('phone').focus();
+       return false;
+   }else{
+       //
+       return true;
+   }
+}
+
 
